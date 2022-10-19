d16e2a34d93346b1e2124899365ea5b7a584071c
ThomasZago <thom.zago@gmail.com>
2022-10-19 20:48:01 +0300

    Project Initialisation

---
+++ Iliyan_Test_Identity_and_Entity_Framework_6/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Iliyan_Test_Identity_and_Entity_Framework_6.Data;
+using Iliyan_Test_Identity_and_Entity_Framework_6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Iliyan_Test_Identity_and_Entity_Framework_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Iliyan_Test_Identity_and_Entity_Framework_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Iliyan_Test_Identity_and_Entity_Framework_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
